a18bf34f7e126a0192902b7c8cc559be8b255c7c
Jeferson <jeferson.silva220@gmail.com>
2024-09-20 21:28:00 -0300

    Add files via upload

---
+++ questao14.js
@@ -0,0 +1,6 @@
+let resposta;
+do{
+    resposta = prompt("Digite 'sim' para continuar:");
+}while (resposta === "sim");
+
+//*Utilize um loop do while que continue pedindo ao usuário para digitar "sim" até que ele digite algo diferente.
